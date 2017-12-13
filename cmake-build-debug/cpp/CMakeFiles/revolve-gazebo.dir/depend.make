bb7f7259eeb43c5bf0ab00e629711607dc4b5940
karinemiras <karine.smiras@gmail.com>
2017-12-13 20:17:42 +0100

    small changes

--- cmake-build-debug/cpp/CMakeFiles/revolve-gazebo.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for revolve-gazebo.
-# This may be replaced when dependencies are built.
--- cmake-build-debug/cpp/CMakeFiles/revolve-spec.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "librevolve-spec.pdb"
-  "librevolve-spec.a"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/revolve-spec.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
